417faafff1e5370c82458fda903234f5d0ff27b6
renechoi <renechoi90@gmail.com>
2023-03-24 10:25:07 +0900

    feat: page converting with web

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -3,9 +3,11 @@ package study.datajpa.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -29,8 +31,8 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public Page<Member> list(Pageable pageable){
-        return memberRepository.findAll(pageable);
+    public Page<MemberDto> list(@PageableDefault(size=5, sort="username") Pageable pageable){
+        return memberRepository.findAll(pageable).map(MemberDto::new);
     }
 
 
@@ -38,6 +40,9 @@ public class MemberController {
     public void init(){
 //        memberRepository.save(new Member("userA"));
 
+        for (int i =0; i< 100; i++){
+            memberRepository.save(new Member("userA"));
+        }
 
     }
 
--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -16,5 +17,11 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+
+    public MemberDto(Member m) {
+        this.id = m.getId();
+        this.username = m.getUsername();
     }
 }
